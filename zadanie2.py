6b48a85672084eb0ed4423f74098d55a903453cc
MichałM <michalhm@gmail.com>
2023-06-05 23:34:06 +0200

    Final

--- zadanie2.py
+++ zadanie2.py
@@ -3,3 +3,9 @@ Popros uzytkownika o podanie zdania.
 Nastepnie oblicz i wyswietl liczbe slow w tym zdaniu. 
 Zakładamy, że słowa są oddzielone spacjami.
 '''
+print("Wprowadz zdanie: ")
+user_input = input("")
+words = user_input.split()
+word_lenght = len(words)
+
+print(word_lenght)
