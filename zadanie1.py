36f8c24b58a1e874e2d8f662ab7447fe2bffb542
MichałM <michalhm@gmail.com>
2023-06-05 23:11:54 +0200

    Final

--- zadanie1.py
+++ zadanie1.py
@@ -3,3 +3,15 @@ Poproś użytkownika o podanie listy liczb całkowitych oddzielonych spacjami.
 Następnie oblicz i wyświetl sumę tych liczb
 '''
 
+user_input = input("Wprowadz liczby calkowite oddzielone spacja: ")
+number_list = user_input.split()
+
+sum = 0
+for calculation in number_list:
+    try:
+        sum += int(calculation)
+    except ValueError:
+        print(f"\"{calculation}\" nie jest liczba calkowita!")
+
+
+print(sum)
