aa52a682830b4008bf98b97f2cdbf9b52a280371
MichałM <michalhm@gmail.com>
2023-06-06 23:50:35 +0200

    alpha

--- zadanie4.py
+++ zadanie4.py
@@ -18,4 +18,12 @@ if isinstance(user_number, int):
     bin_number = bin(user_number)[2:]
     print(bin_number)
 else:
-    print("Liczba z przecinkiem")
+    decimal_part = (user_number - int(user_number))
+    decimal_part = str(decimal_part)
+    decimal_part = decimal_part[2:]
+    decimal_part = int(decimal_part)
+    print(decimal_part)
+    bin_number = bin(int(user_number))[2:]
+    bin_decimal_part = bin(decimal_part)[2:]
+    bin_number = bin_number + "." + bin_decimal_part
+    print(bin_number)
